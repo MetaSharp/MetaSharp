d5dbd7bd7f8d1fa8c651c35cd3574d0dfd7d93bc
James Carroll <jamcar23@gmail.com>
2020-03-04 19:08:56 -0600

    feat(analyzer): add cometary analyzer to project

---
+++ src/MetaSharp.Analyzer/AssemblyLoading.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+#if !APPDOMAINS
+using System.Runtime.Loader;
+#endif
+using Microsoft.CodeAnalysis;
+
+namespace Cometary
+{
+    /// <summary>
+    ///   Manages assembly resolution during the <see cref="CometaryAnalyzer"/> initialization.
+    /// </summary>
+    internal static class AssemblyLoading
+    {
+        public static Dictionary<string, Assembly> Loaded = new Dictionary<string, Assembly>();
+        public static Compilation CurrentCompilation { get; set; }
+
+        public static void EnableResolutionHelp()
+        {
+#if APPDOMAINS
+            AppDomain.CurrentDomain.AssemblyResolve += Resolving;
+#else
+            AssemblyLoadContext.Default.Resolving += Resolving;
+#endif
+        }
+
+        public static void DisableResolutionHelp()
+        {
+#if APPDOMAINS
+            AppDomain.CurrentDomain.AssemblyResolve -= Resolving;
+#else
+            AssemblyLoadContext.Default.Resolving -= Resolving;
+#endif
+        }
+
+
+#if APPDOMAINS
+        private static Assembly Resolving(object sender, ResolveEventArgs args)
+        {
+            var compilation = CurrentCompilation;
+            var assemblyName = new AssemblyName(args.Name);
+
+            if (Loaded.TryGetValue(assemblyName.Name, out Assembly result))
+                return result;
+
+            if (compilation == null)
+                return null;
+
+            foreach (var reference in compilation.References)
+            {
+                if (!(reference is PortableExecutableReference pe) ||
+                    !Path.GetFileNameWithoutExtension(pe.Display).Equals(assemblyName.Name, StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                try
+                {
+                    Assembly assembly = LoadCore(pe.FilePath);
+
+                    Loaded[assemblyName.Name] = assembly;
+
+                    return assembly;
+                }
+                catch
+                {
+                    // ReSharper disable once RedundantJumpStatement
+                    continue;
+                }
+            }
+
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(x => x.GetName().Name == assemblyName.Name && x.GetName().Version == assemblyName.Version);
+        }
+
+        // Wrap the 'Load' logic in another call because of this:
+        // https://github.com/dotnet/coreclr/blob/master/Documentation/botr/type-loader.md#2-type-loader-architecture
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static Assembly LoadCore(string path) => Assembly.LoadFrom(path);
+#else
+        private static Assembly Resolving(AssemblyLoadContext ctx, AssemblyName assemblyName)
+        {
+            Compilation compilation = CurrentCompilation;
+
+            if (Loaded.TryGetValue(assemblyName.Name, out Assembly result))
+                return result;
+
+            if (compilation == null)
+                return null;
+
+            foreach (MetadataReference reference in compilation.References)
+            {
+                if (!(reference is PortableExecutableReference pe) ||
+                    !Path.GetFileNameWithoutExtension(pe.Display).Equals(assemblyName.Name, StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                try
+                {
+                    Assembly assembly = LoadCore(ctx, pe.FilePath);
+
+                    Loaded[assemblyName.Name] = assembly;
+
+                    return assembly;
+                }
+                catch
+                {
+                    // ReSharper disable once RedundantJumpStatement
+                    continue;
+                }
+            }
+
+            return null;
+        }
+
+        // Wrap the 'LoadFromAssemblyPath' logic in another call because of this:
+        // https://github.com/dotnet/coreclr/blob/master/Documentation/botr/type-loader.md#2-type-loader-architecture
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static Assembly LoadCore(AssemblyLoadContext ctx, string path)
+        {
+            using (FileStream fs = File.OpenRead(path))
+            {
+                // Load from a stream instead of loading from path, in order
+                // to avoid locking the file.
+                string pdbFile = Path.ChangeExtension(path, ".pdb");
+
+                if (!File.Exists(pdbFile))
+                    return ctx.LoadFromStream(fs);
+
+                FileStream pdbFs = null;
+
+                try
+                {
+                    pdbFs = File.OpenRead(pdbFile);
+
+                    return ctx.LoadFromStream(fs, pdbFs);
+                }
+                catch
+                {
+                    return ctx.LoadFromStream(fs);
+                }
+                finally
+                {
+                    pdbFs?.Dispose();
+                }
+            }
+        }
+#endif
+    }
+}
---
+++ src/MetaSharp.Analyzer/CometaryAnalyzer.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Cometary
+{
+    /// <summary>
+    ///   <see cref="DiagnosticAnalyzer"/> that initializes a <see cref="CompilationProcessor"/>
+    ///   in this context.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class CometaryAnalyzer : DiagnosticAnalyzer
+    {
+        /// <summary>
+        ///   Describes a <see cref="Diagnostic"/> informing the user that the emitting
+        ///   process has been hooked.
+        /// </summary>
+        public static readonly DiagnosticDescriptor HookSuccess
+#if DEBUG
+            = new DiagnosticDescriptor("HookSuccess", "Hook successful", "The compiler hook has been successfully installed.", Common.DiagnosticsCategory, DiagnosticSeverity.Warning, true);
+#else
+            = new DiagnosticDescriptor("HookSuccess", "Hook successful", "The compiler hook has been successfully installed.", Common.DiagnosticsCategory, DiagnosticSeverity.Info, true);
+#endif
+        /// <summary>
+        ///   Describes a <see cref="Diagnostic"/> informing the user that an error
+        ///   has thrown.
+        /// </summary>
+        public static readonly DiagnosticDescriptor HookError
+            = new DiagnosticDescriptor("HookFailure", "Hook unsuccessful", "{0}", Common.DiagnosticsCategory, DiagnosticSeverity.Error, true, "A registered edit has thrown an exception.");
+
+        /// <inheritdoc />
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+            = ImmutableArray.Create(HookSuccess, HookError);
+
+        public CometaryAnalyzer()
+        {
+            AssemblyLoading.EnableResolutionHelp();
+        }
+
+        /// <inheritdoc />
+        [SuppressMessage("AnalyzerPerformance", "RS1012", Justification = "Yes, all those methods register no action. It's okay.")]
+        public override void Initialize(AnalysisContext context)
+        {
+            // Initialize this exception here
+            // When testing Cometary, I noticed that 'StartAction' often wasn't called at all,
+            // and nothing indicated it (no error, or warning of the sort).
+            // It hasn't happened since I fixed some other stuff (huh R# won't let me use the B u g word)
+            // but I'm leaving this like this, just in case.
+
+            // basically, we default to an error, and iff StartAction is successfully ran, we
+            // remove this error. if it does run an throws, we replace this error with the exception.
+            Exception encounteredException = new Exception("Could not initialize Cometary.");
+
+            void RegisterAction(CompilationStartAnalysisContext ctx)
+            {
+                AssemblyLoading.CurrentCompilation = ctx.Compilation;
+            }
+
+            void StartAction(CompilationStartAnalysisContext ctx)
+            {
+                try
+                {
+                    Hooks.EnsureInitialized();
+
+                    encounteredException = null;
+                }
+                catch (Exception e)
+                {
+                    encounteredException = e;
+                }
+            }
+
+            void EndAction(CompilationAnalysisContext ctx)
+            {
+                ctx.ReportDiagnostic(encounteredException != null
+                    ? Diagnostic.Create(HookError, Location.None, encounteredException.ToString())
+                    : Diagnostic.Create(HookSuccess, Location.None));
+            }
+
+            // Make sure we Cometary.Core can be loaded
+            context.RegisterCompilationStartAction(RegisterAction);
+
+            // Create a matching CometaryManager
+            context.RegisterCompilationStartAction(StartAction);
+
+            // Logs a message on success
+            context.RegisterCompilationAction(EndAction);
+        }
+    }
+}
